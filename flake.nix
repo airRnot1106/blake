5466576a829db703b217fc3d4ee38b4d8c8f069a
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-31 22:28:49 +0900

    chore: init flake.nix

---
+++ flake.nix
@@ -0,0 +1,102 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/70801e06d9730c4f1704fbd3bbf5b8e11c03a2a7";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      fenix,
+      git-hooks,
+      nixpkgs,
+      systems,
+      treefmt-nix,
+      ...
+    }:
+    let
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
+          f {
+            inherit system;
+            pkgs = nixpkgs.legacyPackages.${system};
+            overlays = [
+              fenix.overlays.default
+            ];
+          }
+        );
+      toolchainFor =
+        system:
+        fenix.packages.${system}.stable.withComponents [
+          "cargo"
+          "clippy"
+          "rust-analyzer"
+          "rust-src"
+          "rustc"
+          "rustfmt"
+        ];
+    in
+    {
+      devShells = eachSystem (
+        { system, pkgs, ... }:
+        {
+          default = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            packages = with pkgs; [
+              (toolchainFor system)
+              openssl
+              pkg-config
+            ];
+          };
+        }
+      );
+      formatter = eachSystem (
+        { pkgs, system, ... }:
+        treefmt-nix.lib.mkWrapper pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt.enable = true;
+            rustfmt = {
+              enable = true;
+              package = toolchainFor system;
+            };
+          };
+        }
+      );
+      checks = eachSystem (
+        { system, ... }:
+        {
+          pre-commit-check = git-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              treefmt = {
+                enable = true;
+                package = self.formatter.${system};
+              };
+              clippy = {
+                enable = true;
+                packageOverrides = {
+                  cargo = toolchainFor system;
+                  clippy = toolchainFor system;
+                };
+              };
+            };
+          };
+        }
+      );
+    };
+}
